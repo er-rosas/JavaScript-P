7810324cfadde3efb17647d1fdd99af5f0be0fc6
Ernesto Rosas <er.rosas24@gmail.com>
2024-01-12 22:52:44 -0300

    Basic/0-3

---
+++ JavaScript/Basic/0-3.js
@@ -0,0 +1,17 @@
+// En una variable tienes el lado de un cuadrado, debes escribir un programa que te calcule el área y el perímetro del cuadrado.
+// El area la calculas como lado multiplicado por lado. El perémetro es la suma de los cuatro lados.
+
+// Lado 40
+
+// El area es 1600
+
+// El perímetro es 169
+
+let ladoCuadrado = 40
+
+let area = ladoCuadrado * ladoCuadrado
+let perímetro = ladoCuadrado + ladoCuadrado + ladoCuadrado + ladoCuadrado
+
+console.log("El lado del cuadrado es " + ladoCuadrado)
+console.log("El area del cuadrado es " + area)
+console.log("El perimetro del cuadrado es " + perímetro)

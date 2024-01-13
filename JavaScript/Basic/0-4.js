4216850fb61d1491b516b348abb8f11042da7d2c
Ernesto Rosas <er.rosas24@gmail.com>
2024-01-13 17:56:53 -0300

    Basic/0-4

---
+++ JavaScript/Basic/0-4.js
@@ -0,0 +1,13 @@
+// Un poco de interactividad. En este ejercicio debes escribir un programa que lea el nombre del usuario y salude con un "Hola" seguido del nombre del usuario.
+
+// Para leer el nombre usa un prompt con un texto indicativo como "Escribe tu nombre"
+
+// Aparecerá el prompt y el usuario escribirá su nombre:
+
+// Usuario escribe juan
+
+// Hola Juan.
+
+let nombre = prompt("escribe tu nombre");
+let saludo = "Hola "+nombre;
+console.log(saludo);

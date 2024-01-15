9e5372d248f937d487b24601efc166a4f4a89a79
Ernesto Rosas <er.rosas24@gmail.com>
2024-01-14 23:28:16 -0300

    Basic/0-7

---
+++ JavaScript/Basic/0-7.js
@@ -0,0 +1,17 @@
+// Cambio de unidades. en este ejercicio debes convertir a segundos una medida de tiempo dada en horas y minutos.
+
+// Recuerda una hora son 6o minutos y cada minuto son 60 segundos.
+
+// horas : 2
+
+// minutos 30
+
+// Resultado 2*60*60 + 30*60 = 9000 segundos
+
+let horas = 2
+let minutos = 30
+
+
+let segundos = horas*60*60 + minutos*60
+
+console.log("En este ejercicio hay " + segundos)

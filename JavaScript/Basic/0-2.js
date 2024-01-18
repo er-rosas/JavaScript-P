2524f53d0ebee578cbc46e40663fc57850f11d8d
Ernesto Rosas <er.rosas24@gmail.com>
2024-01-17 23:12:58 -0300

    Basic/condicionales/0-2

--- JavaScript/Basic/0-2.js
+++ JavaScript/Basic/0-2.js
@@ -1,3 +1,5 @@
+//0-1
+
 // Se trata de escribir un script que diga si un número es par o es impar.
 
 // Recordemos que un número es par si al dividirlo por 2 da como resto 0.
@@ -13,3 +15,28 @@ if (parOIMPAR === 0) {
 } else {
     console.log("Es impar")
 }
+
+
+//0-2
+
+// Un programa que pida un número y diga si es positivo o negativo. El cero se considera positivo
+
+// Si num1 es 2 debe decir que num1 es positivo
+
+
+let num02 = prompt("Escribir un numero positivo o negativo")
+
+if (num02 >= 0) {
+    console.log("El numero es positivo")
+} else {
+    console.log("El numero es negativo")
+}
+
+//0-3
+//0-4
+//0-5
+//0-6
+//0-7
+//0-8
+//0-9
+//1-0

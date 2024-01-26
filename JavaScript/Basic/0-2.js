3491ab99db2d2d56e4c438832c6241c4cd200389
Ernesto Rosas <er.rosas24@gmail.com>
2024-01-26 15:44:58 -0300

    Basic/condicionales/1-3

--- JavaScript/Basic/0-2.js
+++ JavaScript/Basic/0-2.js
@@ -216,6 +216,18 @@ console.log(resultado_12)
 
 
 // 1-3
+
+// En ste caso, también usando el perador ternario o if reducido, un visitante a la página deberá teclear su edad, si es igual o mayor de 18 recibirá el mensaje de "Entra" en caso contario deberá decirle los años que tiene que estperar para entrar
+
+// Si le digo que tengo 19 años me dirá "Puedes entrar", si le digo que tengo 14 años me dirá "Espera 4 años"
+
+let edad_13 = parseInt(prompt("Ingrese su edad"))
+let edadRestante13 = 18 - edad_13
+
+let mensaje_13 = (edad_13 >= 18 ) ? "Usted puede entrar" : `Usted debe esperar ${edadRestante13} años para entrar`
+
+console.log(mensaje_13)
+
 // 1-4
 // 1-5
 // 1-6

be99421825ebe9d8719e0f94150515eac89003e8
Ernesto Rosas <er.rosas24@gmail.com>
2024-02-11 20:26:32 -0300

    Basic/bucles-while/1-5/Sin terminar

--- JavaScript/Basic/0-3.js
+++ JavaScript/Basic/0-3.js
@@ -294,6 +294,11 @@ console.log("El número "+ num_14 + esPrimo_14 +" es primo");
 
 
 // 1-5
+
+// Escribir un script que pida al usuario una contraseña. Si coincide con la clave definida en el script le devolverá el mensaje "Acceso concedido" y si no coincide le devolverá el mensaje "Acceso Denegado" . Si falla tres veces se emitirá el mensaje "Alerta, intruso"
+// La contrasñe es por ejemplo pasar. Tecleo tres veces otra distinta: Acceso Intruso.
+
+
 // 1-6
 // 1-7
 // 1-8

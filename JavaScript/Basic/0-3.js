a16c4b09d86b59e0eddf4ade035b8e70aa2ebd7e
Ernesto Rosas <er.rosas24@gmail.com>
2024-03-03 22:27:49 -0300

    Basic/bucles-while/3-0/Sin terminar

--- JavaScript/Basic/0-3.js
+++ JavaScript/Basic/0-3.js
@@ -736,4 +736,22 @@ do {
     num2_29 = parseInt(prompt("2- Ingrese el segundo número de nuevo."))
 } while (num1_29 >= num2_29);
 console.log("Numeros ordenados.");
+
 // 3-0
+
+// Escribir un script para determinar si el numero introducido por el usuario es un palíndromo, es decir, un número que se lee igual del derecho que del revés.
+
+// Para invertir el número podéis hacer sucesivas divisiones entre 10: el resto lo añadis al inveso multiplicado por 10. Y repetis con el valor entero del numero dividido por 10.
+
+// Si escribo el número 12421 me dirá que "12421 es un palíndromo"
+
+// No vale usar strings, solo las operaciones matemáticas necesarias con el número.
+
+let num1_30 = parseInt(prompt("1- Ingrese el primer número."))
+let num2_30 = parseInt(prompt("2- Ingrese el segundo número."))
+
+do {
+    num1_29 = parseInt(prompt("1- Ingrese el primer número de nuevo."))
+    num2_29 = parseInt(prompt("2- Ingrese el segundo número de nuevo."))
+} while (num1_29 >= num2_29);
+console.log("Numeros ordenados.");

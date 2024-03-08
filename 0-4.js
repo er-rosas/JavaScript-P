f95428ad1d524c468335cc538fb77e352b9ddfe7
Ernesto Rosas <er.rosas24@gmail.com>
2024-03-07 23:21:03 -0300

    Basic/funciones/0-1

---
+++ 0-4.js
@@ -0,0 +1,20 @@
+// 0-1
+
+// Crea una función que tenga dos argumentos: num1, num2, y que devuelva como resultado la suma de ambos números. Se supone que se usarán solo números válidos, no letras o símbolos.  
+// Llamo a la función como sumar(3,5) deberá devolver 8.
+
+function sumar(num1, num2) {
+    return num1 + num2
+}
+a = parseInt(prompt("Ingrese el primer número"))
+b = parseInt(prompt("Ingrese el segundo número"))
+console.log(sumar(a,b))
+// 0-2
+// 0-3
+// 0-4
+// 0-5
+// 0-6
+// 0-7
+// 0-8
+// 0-9
+// 1-1

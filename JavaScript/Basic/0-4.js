353acff0b10d3afd31f556d0ede9999e4b220222
Ernesto Rosas <er.rosas24@gmail.com>
2024-03-08 22:27:40 -0300

    Basic/funciones/0-2/Sin terminar

--- JavaScript/Basic/0-4.js
+++ JavaScript/Basic/0-4.js
@@ -9,24 +9,7 @@ function sumar(num1, num2) {
 a = parseInt(prompt("Ingrese el primer número"))
 b = parseInt(prompt("Ingrese el segundo número"))
 console.log(sumar(a,b))
-
 // 0-2
-
-// Esta función se va a llamar esPar(num) y deberá devolver verdadero si el argumento es par y falso si es impar. Se acepta que solo se usarán números válidos.
-// Por ejemplo al llamarla como esPar(5) me dará false, mientras que con esPar(6) me dará true.
-
-
-function esPar(num_02) {
-    if (isNaN(num_02)) {
-        alert("Por favor, introduce un número válido.");
-        // continue;
-    }
-    if (num) {
-        
-    }
-
-}
-num_02 = parseInt(prompt("Ingrese un número"))
 // 0-3
 // 0-4
 // 0-5

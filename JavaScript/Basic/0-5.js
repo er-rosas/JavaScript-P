f7b4337f9466169bfeef12b0ef7b6d46560b28c4
Ernesto Rosas <er.rosas24@gmail.com>
2024-04-05 23:26:24 -0300

    Basic/arrays/0-5/Sin terminar aun

--- JavaScript/Basic/0-5.js
+++ JavaScript/Basic/0-5.js
@@ -118,7 +118,13 @@ console.log("pares " + pares);
 console.log("Impares " + impares);
 
 // 0-5
-//sin terminar
+// Escribe una función que pueda devolver el valor más pequeño de un array numérico o el índice de ese valor. El segundo parámetro de la función dirá si debe devolver el valor ("v") o el índice ("i"). No uses métodos del objeto array.
+// Se supone que el primer parámetro siempre será una matriz llena con al menos 1 número y sin duplicados.
+// Se suponer el segundo parámetro será una letra con uno de dos valores: 'v' 'i'
+// min([4,5,7], 'v')  devolverá 4, mientras que min([4,5,7], 'i') devolverá 0.
+
+let array05 = []
+
 // 0-6
 // 0-7
 // 0-8

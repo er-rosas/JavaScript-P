69530a997495930adb2e65935bcf262b6e708ecf
Ernesto Rosas <er.rosas24@gmail.com>
2024-04-20 23:54:34 -0300

    Basic/arrays/0-9/Sin terminar

--- JavaScript/Basic/0-5.js
+++ JavaScript/Basic/0-5.js
@@ -225,4 +225,13 @@ function listar08(array08) {
 
 console.log(listar08(["rojo", "verde", "azul"]))
 // 0-9
+
+// Un servicio de atención al ciente tiene establecido turnos semanales para sus empleados de manera que cada día de la semana se encarga del servicio  una persona: lunes - María, Martes - Luis, Miércoles - Antonia, Jueves - Pedro, Viernes - Marisa.
+
+// Usa un array para almacenar los datos del servicio. En este array cada elemento será un par día - nombre.
+
+// Crea una función que responda con el nombre de la persona encargada del servicio sabiendo el día. Si el día no existe deberá decir que no hay servicio. Los nombres de los días deben ir en minúsculas.
+// Si le pido servicio('Lunes') me debería decir "Este día se encarga María" y si le pido servico(Miércoles") me daría Antonia.
+
+
 // 1-0

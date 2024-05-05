b2f27e1f88b8ad2a46976eb3b84e8da3076be977
Ernesto Rosas <er.rosas24@gmail.com>
2024-05-04 23:45:49 -0300

    Basic/clases-objetos/0-3/Sin terminar

--- JavaScript/Basic/0-6.js
+++ JavaScript/Basic/0-6.js
@@ -54,7 +54,15 @@ vestido = {
 vestido.precio = parseInt(prompt("Precio bruto "));
 vestido.descuento = parseInt(prompt("Precio bruto "));
 console.log("El precio neto es "+vestido.neto()+"€");
+
 // 0-3
+
+// En esta propuesta debes crear una clase de nombre piedra (con class o con function) con dos propiedades: masa, volumen y un método densidad que calcule este valor (masa/volumen). Luego instancia al menos un objeto de esta clase, dale un valor a su masa y a su volumen y ejecuta el método densidad.
+// Por ejemplo objeto oro, masa de la muestra 194 gr y volumen 10 cc. Densidad deberá devolver 19,4.
+
+class piedra {
+    
+}
 // 0-4
 // 0-5
 // 0-6
